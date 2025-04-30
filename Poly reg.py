ec80550f83cf002bfb07d46793d3956146efd7d2
Sanal-T <sanalt2024@gmail.com>
2025-04-30 22:46:19 +0530

    Intialised libraries

---
+++ Poly reg.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+from sklearn.model_selection import train_test_split 
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures 
+from sklearn.linear_model import LinearRegression 
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
